aa6c529d181db87039712a9c214609cf9c723dcf
LABELSCOIN <lbsc@MAC-AIR-LABELSCOIN-2-FERNANDO.local>
2020-08-06 14:05:39 +0200

    gfgh

---
+++ src/app/view-users/view-users.page.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AngularFireAuth } from '@angular/fire/auth';
+@Component({
+  selector: 'app-view-users',
+  templateUrl: './view-users.page.html',
+  styleUrls: ['./view-users.page.scss'],
+})
+export class ViewUsersPage implements OnInit {
+  slideOpts = {
+    initialSlide: 0,
+    speed: 400
+  };
+  constructor(
+    public  afAuth: AngularFireAuth,
+    private readonly router: Router,
+  ) { }
+  ngOnInit() {
+  }
+  async logout(){
+    await this.afAuth.signOut();
+    this.router.navigateByUrl('/login-register');
+  }
+}
