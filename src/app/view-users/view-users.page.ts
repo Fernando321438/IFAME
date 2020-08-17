4ca5ed5b75d0d7f8c0926d06afe4ab567af1c3ab
LABELSCOIN <lbsc@MAC-AIR-LABELSCOIN-2-FERNANDO.local>
2020-08-17 10:28:12 +0200

    v10

--- src/app/view-users/view-users.page.ts
+++ src/app/view-users/view-users.page.ts
@@ -1,13 +1,17 @@
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import { AngularFireAuth } from '@angular/fire/auth';
-import { IonSlides, IonSlide } from '@ionic/angular';
-import { AngularFirestore } from '@angular/fire/firestore';
-import { AngularFireStorage } from '@angular/fire/storage';
+import { Component, OnInit } from "@angular/core";
+import { Router, ActivatedRoute } from "@angular/router";
+import { AngularFireAuth } from "@angular/fire/auth";
+import { IonSlides, IonSlide } from "@ionic/angular";
+import { AngularFirestore } from "@angular/fire/firestore";
+import { AngularFireStorage } from "@angular/fire/storage";
+import { AngularFireDatabase } from "@angular/fire/database";
+import { Media, MediaObject } from "@ionic-native/media/ngx";
+import {LivePage} from "../live/live.page";
+
 @Component({
-  selector: 'app-view-users',
-  templateUrl: './view-users.page.html',
-  styleUrls: ['./view-users.page.scss'],
+  selector: "app-view-users",
+  templateUrl: "./view-users.page.html",
+  styleUrls: ["./view-users.page.scss"],
 })
 export class ViewUsersPage implements OnInit {
   selectedSlide: any;
@@ -15,50 +19,63 @@ export class ViewUsersPage implements OnInit {
   immagine = [];
 
   segment = 0;
-  sliderOptions={
-    initialSlide:0,
+  sliderOptions = {
+    initialSlide: 0,
     slidesPerView: 1,
-    speed: 400
-  }
+    speed: 400,
+  };
   /* slideOpts = {
     initialSlide: 0,
     speed: 400 */
+ 
+  records: any[0];
   
-    records:any[0];
-    artistCurrent: any[0];
+  imgURL:any=[];
+
   constructor(
-    public  afAuth: AngularFireAuth,
-    private afStorage:AngularFireStorage,
+
+    public afAuth: AngularFireAuth,
+    private afStorage: AngularFireStorage,
     private afs: AngularFirestore,
+    private activatedRoute: ActivatedRoute,
+    private afDB: AngularFireDatabase,
+    private authObj: AngularFireAuth,
+    private media: Media,
     private readonly router: Router,
+    
   ) {
- 
- 
+   
+   }
+
+  async ngOnInit() {
+  this.immagineLive();
   
    
-     afs.collection('/artist/').valueChanges()
+    
+  /*  this.afs.collection('/artist/').doc ('/' +(await this.afAuth.currentUser).uid).snapshotChanges().subscribe((usersnap: any) => {
+      this.user = { 'imgURL': usersnap.imgURL, ...usersnap.payload.val() };
+
+    })
+    this.immagine.push({
+      url: this.user.imgURL,
+      
+    }) 
+   */
+  }
+
+  async immagineLive(){
+    this.afs.collection('/artist/').valueChanges()
     .subscribe(records => {
       this.records = records;
       console.log(this.records);
 
     })  
-    
-   }
-  async ngOnInit() {
-    this.afStorage.ref('/Live/'+ (await this.afAuth.currentUser).uid)
+  /*   this.afStorage.ref('/Live/'+ (await this.afAuth.currentUser).uid + this.artistCurrent.recordname)
     .getDownloadURL().subscribe(url=> {
  
       this.artistCurrent.imgURL= url ;
-      console.log(url);
-    })
-   this.afStorage.ref('/Live/' + (await this.afAuth.currentUser).uid).getDownloadURL().subscribe((usersnap: any) => {
-      this.user = { 'imgURL': usersnap.imgURL, ...usersnap.payload.val() };
-      console.log(this.user);
-
-    })
-    this.immagine.push({
-      url: this.user.imgURL
-    })
+      console.log(this.artistCurrent.imgURL);
+    }) */
   }
 
   async segmentChanged(ev){
