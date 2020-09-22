db74dba38dce3017a3fb2c67a1a5fd8b667096d5
LABELSCOIN <lbsc@MAC-AIR-LABELSCOIN-2-FERNANDO.local>
2020-09-22 13:15:24 +0200

    gdgd

--- src/app/view-users/view-users.page.ts
+++ src/app/view-users/view-users.page.ts
@@ -8,6 +8,7 @@ import { AngularFireDatabase } from "@angular/fire/database";
 import { Media, MediaObject } from "@ionic-native/media/ngx";
 import {LivePage} from "../live/live.page";
 import { first } from 'rxjs/operators';
+import { DataService } from "../services/data.service";
 
 @Component({
   selector: "app-view-users",
@@ -15,7 +16,9 @@ import { first } from 'rxjs/operators';
   styleUrls: ["./view-users.page.scss"],
 })
 export class ViewUsersPage implements OnInit {
-  
+  public searchTerm: string = "";
+  public items: any;
+
   selectedCards: any;
   user: any = {};
   immagine = [];
@@ -31,15 +34,15 @@ export class ViewUsersPage implements OnInit {
     initialSlide: 0,
     speed: 400 */
   recordname:any;
-  records: any[0];
-  songs: any[0];
+  records: any;
+  songs: any;
   artistCurrent: any[0];
   imgURL:any=[];
   
   live:boolean=true;
   music:boolean=false;
   search:boolean=false;
-
+  isSearch= false;
   constructor(
     public afAuth: AngularFireAuth,
     private afStorage: AngularFireStorage,
@@ -50,55 +53,38 @@ export class ViewUsersPage implements OnInit {
     private authObj: AngularFireAuth,
     private media: Media,
     private readonly router: Router,
-    
+    private dataService: DataService
   ) {
+    
+      this.afs.collection('/Live Record').valueChanges()
+      .subscribe(records => {
+        this.records = records;
+        console.log(this.records);
+  
+      })  
+      
+     this.afs.collection('/Digital Record').valueChanges()
+      .subscribe(songs => {
+        this.songs = songs;
+        console.log(this.songs);
+  
+      })  
+    
  
 
    
-     afs.collection('/Live Record').valueChanges()
-    .subscribe(records => {
-      this.records = records;
-      console.log(this.records);
-
-    })  
-    
-    afs.collection('/Digital Record').valueChanges()
-    .subscribe(songs => {
-      this.songs = songs;
-      console.log(this.songs);
-
-    })  
+   
    }
   async ngOnInit() {
   
-  
-
+    this.setFiltered();
+}
+setFiltered() {
+  this.records = this.dataService.filterRecords(this.searchTerm);
+  this.songs = this.dataService.filterSongs(this.searchTerm);
 }
 
 
-async initializeItems()
-{
-  this.records.imgURL =[];
-  this.records.Recordname=[];
-
-  this.songs.Songname=[];
-  this.songs.imgURL=[];
-  
-
-}
-  
-filterList(searchTerm) {
-  return this.records.filter(record => {
-    return record.Recordname.indexOf(searchTerm.toLowerCase()) > -1;
-  });
-  
-}
-filterList2(searchTerm) {
-  return this.songs.filter(song => {
-    return song.Songname.indexOf(searchTerm.toLowerCase()) > -1;
-  });
-  
-}
 
 
   segmentChanged(event)
