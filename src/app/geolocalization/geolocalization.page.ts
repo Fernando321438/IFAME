f1e4917f111ee27c036278a0897526fb5bd0ebc4
fernando <63153860+iFame@users.noreply.github.com>
2021-02-06 01:57:39 +0100

    Prototipo1

---
+++ src/app/geolocalization/geolocalization.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-geolocalization',
+  templateUrl: './geolocalization.page.html',
+  styleUrls: ['./geolocalization.page.scss'],
+})
+export class GeolocalizationPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
