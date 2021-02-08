eea7bfafcd226d5af1e7abb563e2a87db89a3dde
fernando <63153860+iFame@users.noreply.github.com>
2021-02-08 11:50:39 +0100

    fdskjfljkds

---
+++ android/app/src/main/kotlin/com/example/shazam/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.Shoma
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/home.dart
@@ -0,0 +1,70 @@
+import 'package:avatar_glow/avatar_glow.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:hooks_riverpod/all.dart';
+import 'package:Shoma/song_screen.dart';
+import 'package:Shoma/viewmodels/home_vm.dart';
+
+class HomePage extends HookWidget {
+  @override
+  Widget build(BuildContext context) {
+    final vm = useProvider(homeViewModel);
+    return ProviderListener<HomeViewModel>(
+      provider: homeViewModel,
+      onChange: (context, vm) {
+        if (vm.success) {
+          Navigator.push(
+              context,
+              MaterialPageRoute(
+                  builder: (context) => SongScreen(
+                        song: vm.currentSong,
+                      )));
+        }
+      },
+      child: Scaffold(
+        backgroundColor: Color(0xFF7d8183),
+        body: Container(
+            height: MediaQuery.of(context).size.height,
+            width: MediaQuery.of(context).size.width,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Text(
+                  'Tap to Shoma',
+                  style: TextStyle(color: Colors.white, fontSize: 20),
+                ),
+                SizedBox(
+                  height: 40,
+                ),
+                AvatarGlow(
+                  endRadius: 200.0,
+                  animate: vm.isRecognizing,
+                  child: GestureDetector(
+                    onTap: () => vm.isRecognizing
+                        ? vm.stopRecognizing()
+                        : vm.startRecognizing(),
+                    child: Material(
+                      shape: CircleBorder(),
+                      elevation: 8,
+                      child: Container(
+                        padding: EdgeInsets.all(40),
+                        height: 250,
+                        width: 250,
+                        decoration: BoxDecoration(
+                            shape: BoxShape.circle, color: Color(0xFF7d8183)),
+                        child: Image.asset(
+                          'assets/images/shazam-logo.png',
+                          height: 350,
+                          width: 350,
+                        ),
+                      ),
+                    ),
+                  ),
+                )
+              ],
+            )),
+      ),
+    );
+  }
+}
